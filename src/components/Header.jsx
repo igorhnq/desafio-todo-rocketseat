af964e80c4c786270cbcee5abe93305374c4b4c2
igorhnq <igorhenrique102@gmail.com>
2025-02-15 20:46:48 -0300

    feat: add Header component

---
+++ src/components/Header.jsx
@@ -0,0 +1,12 @@
+import todoLogo from '../assets/todo-logo.svg'
+
+import styles from './Header.module.css'
+
+export function Header() {
+    return (
+        <header className={styles.header}>
+            <img src={todoLogo} />
+            <h1></h1>
+        </header>
+    )
+}
