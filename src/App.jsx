3f509f08834d8b98f6d9e90c24924e9aac30b092
igorhnq <igorhenrique102@gmail.com>
2025-02-17 16:36:28 -0300

    refactor: import Task component

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import { Input } from './components/Input'
 import styles from './App.module.css'
 
 import './global.css'
+import { Task } from './components/Task'
 
 export function App() {
     return (
@@ -11,6 +12,19 @@ export function App() {
             <Header />
             <div className={styles.wrapper}>
                 <Input />
+                <div className={styles.header}>
+                    <div>
+                        <h2>Tarefas criadas</h2>
+                        <p>5</p>
+                    </div>
+
+                    <div>
+                        <h2>Concluídas</h2>
+                        <p>2 de 5</p>
+                        <p>5</p>
+                    </div>
+                </div>
+                <Task />
             </div>
         </div>
     )
